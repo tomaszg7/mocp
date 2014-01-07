97ef14f1e850e391699725c6f832c978274c1b99
tomaszg <tomaszg@alpha.uwb.edu.pl>
2014-01-07 05:38:03 +0100

    Initial attempt to put non-float SFMT into sndlib decoder. Assuming sizeof(int)=4.

--- decoder_plugins/sndfile/sndfile.c
+++ decoder_plugins/sndfile/sndfile.c
@@ -115,11 +115,28 @@ static int sndfile_decode (void *void_data, char *buf, int buf_len,
 
 	sound_params->channels = data->snd_info.channels;
 	sound_params->rate = data->snd_info.samplerate;
-	sound_params->fmt = SFMT_FLOAT;
 
-	return sf_readf_float (data->sndfile, (float *)buf,
-			buf_len / sizeof(float) / data->snd_info.channels)
-		* sizeof(float) * data->snd_info.channels;
+#ifdef INTERNAL_FLOAT
+	switch (data->snd_info.format & SF_FORMAT_SUBMASK) {
+		case SF_FORMAT_FLOAT:
+		case SF_FORMAT_DOUBLE:
+		case SF_FORMAT_VORBIS:
+			sound_params->fmt = SFMT_FLOAT;
+			return sf_readf_float (data->sndfile, (float *)buf,
+			    buf_len / sizeof(float) / data->snd_info.channels)
+			    * sizeof(float) * data->snd_info.channels;
+		default:
+			sound_params->fmt = SFMT_S32 | SFMT_NE;
+			return sf_readf_int (data->sndfile, (int *)buf,
+			    buf_len / sizeof(int) / data->snd_info.channels)
+			    * sizeof(int) * data->snd_info.channels;
+		}
+#else
+	sound_params->fmt = SFMT_S32 & SFMT_NE;
+	return sf_readf_int (data->sndfile, (int *)buf,
+	    buf_len / sizeof(int) / data->snd_info.channels)
+	    * sizeof(int) * data->snd_info.channels;
+#endif
 }
 
 static int sndfile_get_bitrate (void *void_data ATTR_UNUSED)
