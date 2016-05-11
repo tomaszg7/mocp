5ef4c5315d693b0a2361389230f2e854fcee032c
Tomasz Golinski <tomaszg@math.uwb.edu.pl>
2016-05-11 15:40:06 +0200

    Flac decoder: correct computation of avg_bitrate

--- decoder_plugins/flac/flac.c
+++ decoder_plugins/flac/flac.c
@@ -247,6 +247,8 @@ static void *flac_open_internal (const char *file, const int buffered)
 	data->length = -1;
 	data->ok = 0;
 
+        off_t file_size;
+
 	data->stream = io_open (file, buffered);
 	if (!io_ok(data->stream)) {
 		decoder_error (&data->error, ERROR_FATAL, 0,
@@ -280,7 +282,12 @@ static void *flac_open_internal (const char *file, const int buffered)
 	}
 
 	data->ok = 1;
-	data->avg_bitrate = data->bits_per_sample * data->sample_rate;
+
+//      Wrong calculation, produces bitrate of uncompressed stream for one channel
+//	data->avg_bitrate = data->bits_per_sample * data->sample_rate;
+        file_size = io_file_size (data->stream);
+        if (data->length > 0 && file_size != -1)
+                data->avg_bitrate = file_size / data->length * 8;
 
 	debug ("File opened. Channels %d. Samplerate %d. Duration %d. Avg_Bitrate %d.", data->channels, data->sample_rate,
 		data->length, data->avg_bitrate);
