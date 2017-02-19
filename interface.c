e6cc464797c29f4af7c1e99c71f9680d6dc5188c
Tomasz Golinski <tomaszg@math.uwb.edu.pl>
2017-02-19 22:11:58 +0100

    If cwd is no longer available during refresh, jump to ..

--- interface.c
+++ interface.c
@@ -2081,7 +2081,8 @@ static void toggle_show_format ()
 /* Reread the directory. */
 static void reread_dir ()
 {
-	go_to_dir (NULL, 1);
+	while (go_to_dir (NULL, 1)==0)
+		go_dir_up();
 }
 
 /* Clear the playlist on user request. */
